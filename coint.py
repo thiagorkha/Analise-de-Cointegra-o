3d0e212ce70072ec6ada6bb4dca974beaf1491da
thiagorkha <91204179+thiagorkha@users.noreply.github.com>
2021-11-13 10:23:44 -0300

    Update coint.py

--- coint.py
+++ coint.py
@@ -1,6 +1,6 @@
 import streamlit as st
 #import plotly.express as px
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 import base64
 from io import StringIO
 from io import BytesIO
