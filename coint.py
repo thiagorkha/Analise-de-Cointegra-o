47f02a23169f5f2417727229fe08443b36e9d3c4
thiagorkha <91204179+thiagorkha@users.noreply.github.com>
2021-11-13 12:27:32 -0300

    Update coint.py

--- coint.py
+++ coint.py
@@ -15,6 +15,8 @@ import plotly.graph_objects as go
 import ibov
 from funcoes import coint_model, get_market_data, half_life, get_beta_plot, _get_residuals_plot, beta_rotation, asBase64, fp_savefig, st_get_residuals_plot, st_get_beta_plot,beta_rotation1, clean_timeseries, drop_nan, coint_model1, gera_pares, download_hquotes, coint_model2, get_residuals_plot1, get_beta_plot1
 
+st.set_page_config(page_title="Plataforma de Cointegração",layout='wide')
+
 senha = st.sidebar.text_input("Password:", value="", type="password")
 senha_sl = 'thiago123'
 
