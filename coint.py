5a480feda3487ff2d07865f46f3c6b9639709467
thiagorkha <91204179+thiagorkha@users.noreply.github.com>
2021-11-26 14:08:45 -0300

    Update coint.py

--- coint.py
+++ coint.py
@@ -32,7 +32,7 @@ if senha == senha_sl:
   pag = st.sidebar.selectbox('Escolha uma Opção',['Início', 'Análise','Buscar pares'], 0)
 
   if pag == 'Início':
-    st.image('Logo.jpg', width=100)
+    st.image('Logo.jpg', width=50)
     st.title('Plataforma de Cointegração')
 
 
