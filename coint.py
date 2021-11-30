225cdff618bd4b1bf58478ca1ec59af9346abb3a
thiagorkha <91204179+thiagorkha@users.noreply.github.com>
2021-11-30 15:11:07 -0300

    Update coint.py

--- coint.py
+++ coint.py
@@ -61,7 +61,7 @@ if senha == senha_sl:
 
       for par in pares1:
         for periodo in periodos:
-            coint = coint_model1(par[0], par[1], periodo)
+            coint = coint_model2(par[0], par[1], periodo, quote)
             Adfr = coint['ADF']
             hl, _ = half_life((coint['OLS']).resid)
             residuo = (coint['OLS']).resid
